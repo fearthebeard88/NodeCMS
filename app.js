96c0196baee37c5baf34fe64c1af74358ee62382
fearthebeard88 <tferris88@gmail.com>
2019-01-20 22:23:58 -0500

    set up server, working on route

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const handlebars = require('express-handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', handlebars({defaultLayout: 'home'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res)=>
+{
+    res.render('home/index');
+})
 
 app.listen(8000, ()=>
 {
