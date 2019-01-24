a8d8529b55d3cb994b4779ea549c3b56af151dc3
fearthebeard88 <tferris88@gmail.com>
2019-01-23 20:48:34 -0500

    shenanigans

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+// tells server to serve static content (css and js files) from public
+// directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// telling server what the template engine and extension is, and
+// setting default layout file
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
+
+// assigning a name to a value, kind of like a registry
+// some names are reserved for the express app
 app.set('view engine', 'handlebars');
 
+// requiring router files for home and admin
 const home = require('./routes/home/index.js');
 const admin = require('./routes/admin/index.js');
+
+// tells the server when the base of the request matches one of these
+// patterns to use the correct router based off the base of the path
 app.use('/', home);
 app.use('/admin', admin);
 
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+// tells the server to apply logic to all requests that match
+// the base of this route
 router.all('/*', (req, res, next)=>
 {
-    
-})
+    // req.app holds reference to instance of server that is using 
+    // middleware
+    req.app.locals.layout = 'admin';
+    next();
+});
 
 // url actually looks like localhost.local/admin/
+// using middleware to tell express when /admin is requested to use this
+// router instead of the home router
 router.get('/', (req, res)=>
 {
     res.render('admin/index');
 });
+
+router.get('/dashboard', (req, res)=>
+{
+    res.render('admin/dashboard');
+})
 
 module.exports = router;
