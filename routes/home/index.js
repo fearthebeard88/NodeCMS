282747f8e85c77bc3eba33e50cafbd5d5eb2c567
fearthebeard88 <tferris88@gmail.com>
2019-02-16 16:23:51 -0500

    created new route and page for displaying single post on frontend

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Post');
 
 router.all('/*', (req, res, next)=>
 {
@@ -9,7 +10,13 @@ router.all('/*', (req, res, next)=>
 
 router.get('/', (req, res)=>
 {   
-    res.render('home/index');
+    Post.find({}).then(posts=>
+    {
+        res.render('home/index', {posts:posts});
+    }).catch(err=>
+    {
+        res.status(500).send('<h1>Error loading page</h1><br/><p>' + err + '</p>');
+    });
 });
 
 router.get('/about', (req, res)=>
@@ -25,6 +32,18 @@ router.get('/login', (req, res)=>
 router.get('/register', (req, res)=>
 {
     res.render('home/register');
+});
+
+router.get('/post/:id', (req, res)=>
+{
+    Post.findById(req.params.id).then(post=>
+    {
+        res.render('home/posts/post', {post: post});
+    }).catch(err=>
+    {
+        console.log(`Unable to locate post with id ${req.params.id}`);
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
